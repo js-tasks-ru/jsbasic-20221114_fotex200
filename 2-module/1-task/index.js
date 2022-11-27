8e5d33f32d38502436da138c1a19da553bab46cc
marinabatch26 <116509336+marinabatch26@users.noreply.github.com>
2022-11-27 12:23:50 -0500

    Homework module #2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+
+  for (let key in salaries) {
+    const salaryValue = salaries[key];
+    if (typeof salaryValue == 'number' && isFinite(salaryValue)) {
+      sum += salaryValue;
+    }
+  }
+  return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,18 @@
 let calculator = {
   // ваш код
+  a: 0,
+  b: 0,
+
+  read(a, b) {
+    this.a = +a;
+    this.b = +b;
+  },
+  sum(a, b) {
+    return (this.a + this.b);
+  },
+  mul(a, b) {
+    return (this.a * this.b);
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
